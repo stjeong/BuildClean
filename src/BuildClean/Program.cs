477d5f1c2c27114706a79c1f8165e09798d8a577
kevin <kevin@THE10>
2017-01-03 23:50:30 +0900

    Add project files.

---
+++ src/BuildClean/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            char separator = Path.DirectorySeparatorChar;
+
+            foreach (string dirPath in Directory.EnumerateDirectories(Directory.GetCurrentDirectory(), "*.*", SearchOption.AllDirectories))
+            {
+                if (dirPath.EndsWith(separator + "bin", StringComparison.CurrentCultureIgnoreCase) == true
+                    || dirPath.EndsWith(separator + "obj", StringComparison.CurrentCultureIgnoreCase) == true)
+                {
+                    try
+                    {
+                        foreach (string filePath in Directory.GetFiles(dirPath, "*.*", SearchOption.AllDirectories))
+                        {
+                            try
+                            {
+                                File.Delete(filePath);
+                            }
+                            catch { }
+                        }
+
+                        Directory.Delete(dirPath, true);
+                    }
+                    catch { }
+                }
+            }
+        }
+    }
+}
